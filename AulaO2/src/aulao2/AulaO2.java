1fc073976cb0f631ffc022e9990368ed3cabe2d6
Moisés Felix da Silva <moises012felix@gmail.com>
2023-12-08 15:40:20 -0300

    teste commit

--- AulaO2/src/aulao2/AulaO2.java
+++ AulaO2/src/aulao2/AulaO2.java
@@ -15,6 +15,9 @@ public class AulaO2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("OLLAAAA");
+        
         Caneta c1 = new Caneta();
         c1.cor = "azul";
         c1.ponta = 0.5f;
